b9f2afe6744bcf4ffae09f1bfc87e43c485530bb
Orçun Uyar <pejkopat@gmail.com>
2025-06-07 18:18:18 +0300

    fix: Replace __dirname with process.cwd() in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ import path from 'path'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   sassOptions: {
-    includePaths: [path.join(__dirname, 'node_modules')],
+    includePaths: [path.join(process.cwd(), 'node_modules')],
   },
   webpack: (config) => {
     config.module.rules.push({
