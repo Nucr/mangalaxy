c5b671485fd6084eac94c7fa17f25be72d8e4580
Orçun Uyar <pejkopat@gmail.com>
2025-06-08 12:05:32 +0300

    Fix: ChapterDetailPageProps type error in app/manga/[mangaId]/chapter/[bolumNumarasi]/page.tsx

--- app/manga/[mangaId]/chapter/[bolumNumarasi]/page.tsx
+++ app/manga/[mangaId]/chapter/[bolumNumarasi]/page.tsx
@@ -3,7 +3,7 @@ import { slugify } from '@/lib/utils';
 import { fetchAnilistData, GET_RECENT_MANGAS, AnilistResponse } from '@/lib/anilist';
 
 interface ChapterDetailPageProps {
-  params: { mangaId: string; bolumNumarasi: string };
+  params: Promise<{ mangaId: string; bolumNumarasi: string }>;
 }
 
 export async function generateStaticParams() {
@@ -24,7 +24,7 @@ export async function generateStaticParams() {
 }
 
 export default async function ChapterDetailPage(props: ChapterDetailPageProps) {
-  const { mangaId, bolumNumarasi } = props.params;
+  const { mangaId, bolumNumarasi } = await props.params;
 
   return (
     <div className="container mx-auto px-4 py-8">
